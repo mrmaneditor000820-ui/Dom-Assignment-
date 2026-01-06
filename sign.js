f0dfaaeeaab461e52decdf12eab7a2910e6edce2
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-01-06 19:28:57 +0500

    some

--- sign.js
+++ sign.js
@@ -1,73 +1,73 @@
-  //   let count = 0;
-    // function increase() {
-    //   count++;
-    //   document.getElementById("count").innerText = count;
-    // }
-    // function decrease() {
-    //   count--;
-    //   document.getElementById("count").innerText = count;
-    // }
-    // function reset() {
-    //   count = 0;
-    //   document.getElementById("count").innerText = count;
-    // }
+//   let count = 0;
+// function increase() {
+//   count++;
+//   document.getElementById("count").innerText = count;
+// }
+// function decrease() {
+//   count--;
+//   document.getElementById("count").innerText = count;
+// }
+// function reset() {
+//   count = 0;
+//   document.getElementById("count").innerText = count;
+// }
 
-    var count = 0;
-    function increase(action){
-      switch (action) {
-        case 'increase':
-          count++;
-            break;
-       case 'decrease':
-        count--;
-        break;
-        case 'reset':
-          count = 0;
-          break;
-      }
-       document.getElementById('count').innerText = count;
+var count = 0;
+function increase(action) {
+  switch (action) {
+    case 'increase':
+      count++;
+      break;
+    case 'decrease':
+      count--;
+      break;
+    case 'reset':
+      count = 0;
+      break;
+  }
+  document.getElementById('count').innerText = count;
 
+}
+
+function showPassWord() {
+  var passwordField = document.getElementById("password");
+  var showPasswordIcon = document.getElementById("showPassword");
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+
+  }
+  else {
+    passwordField.type = "password";
+  }
+}
+
+function checkNumber() {
+  var number = document.getElementById("number").value;
+  var message = document.getElementById("para");
+  console.log("number==>", number);
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      message.innerText = number + " is not a prime number" + number;
+      return;
     }
-
-    function showPassWord() {
-      var passwordField = document.getElementById("password");
-      var showPasswordIcon = document.getElementById("showPassword");
-        if (passwordField.type === "password") {
-            passwordField.type = "text";
-
-        }
- else {
-            passwordField.type = "password";
-        }
-           }
-
-function checkNumber(){
-var number = document.getElementById("number").value;
-var message =document.getElementById("para");
-console.log("number==>",number);
-for (let i=2;  i<number; i++){
-  if(number%i ==0){
-    message.innerText= number + " is not a prime number" + number;
-    return;
-  } 
-}
-message.innerText= number + " is a prime number" + number; 
+  }
+  message.innerText = number + " is a prime number" + number;
 
 }
 
-function checkOddEven(){
+function checkOddEven() {
   var number = document.getElementById("oddEven").value;
-  var message =document.getElementById("paragraph");
-  console.log("number==>",number);
-  for (let i=2;  i<number; i++){
-  
-  if(number % 2 ==0){
-    message.innerText= number + " is Even number" + number;
-    return;
-  } 
-}
-  message.innerText= number + " is Odd number" + number; 
-  
+  var message = document.getElementById("paragraph");
+  console.log("number==>", number);
+  for (let i = 2; i < number; i++) {
+
+    if (number % 2 == 0) {
+      message.innerText = number + " is Even number" + number;
+      return;
+    }
+  }
+  message.innerText = number + " is Odd number" + number;
+
 }
 
 
